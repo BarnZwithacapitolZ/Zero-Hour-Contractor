614e24a0189f8068d36a0d2987e7ab49222fd9a4
sam barnes <barnes.samb@gmail.com>
2018-09-13 17:56:34 +0100

    Add updated sql for database

--- zerocontractor.sql
+++ zerocontractor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 09, 2018 at 10:04 PM
+-- Generation Time: Sep 13, 2018 at 04:55 PM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -39,35 +39,35 @@ CREATE TABLE IF NOT EXISTS `tblbook` (
   PRIMARY KEY (`BookID`),
   UNIQUE KEY `EmployeeID` (`EmployeeID`,`DepartmentID`,`BookID`),
   KEY `DepartmentID` (`DepartmentID`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `tblbook`
+-- Table structure for table `tblcompany`
 --
 
-INSERT INTO `tblbook` (`EmployeeID`, `DepartmentID`, `BookID`, `StartTime`, `EndTime`, `BookDate`) VALUES
-(2, 1, 1, '05:45:00', '06:00:00', '2018-08-13'),
-(2, 2, 2, '13:00:00', '18:00:00', '2018-08-14'),
-(2, 1, 3, '07:00:00', '16:00:00', '2018-08-15'),
-(2, 2, 4, '05:45:00', '12:00:00', '2018-08-17'),
-(2, 3, 5, '07:00:00', '14:00:00', '2018-08-16'),
-(3, 3, 6, '18:00:00', '22:00:00', '2018-08-14'),
-(3, 2, 7, '16:00:00', '22:00:00', '2018-08-16'),
-(3, 3, 8, '12:00:00', '18:00:00', '2018-08-19'),
-(4, 3, 9, '07:00:00', '12:00:00', '2018-08-14'),
-(4, 3, 10, '13:00:00', '18:00:00', '2018-08-16'),
-(4, 3, 11, '07:00:00', '12:00:00', '2018-08-17'),
-(4, 2, 12, '08:00:00', '12:00:00', '2018-08-18'),
-(5, 3, 13, '18:00:00', '22:00:00', '2018-08-17'),
-(5, 3, 14, '18:00:00', '22:00:00', '2018-08-18'),
-(5, 2, 15, '06:45:00', '12:00:00', '2018-08-19'),
-(2, 3, 16, '16:00:00', '22:00:00', '2018-08-13'),
-(5, 1, 17, '09:00:00', '17:00:00', '2018-08-19'),
-(6, 2, 18, '00:00:00', '23:00:00', '2018-08-14'),
-(5, 2, 19, '09:00:00', '16:00:00', '2018-08-19'),
-(1, 2, 20, '05:00:00', '23:00:00', '2018-08-22'),
-(1, 1, 21, '04:00:00', '11:00:00', '2018-09-02'),
-(1, 1, 22, '18:00:00', '23:00:00', '2018-09-02');
+DROP TABLE IF EXISTS `tblcompany`;
+CREATE TABLE IF NOT EXISTS `tblcompany` (
+  `CompanyID` int(11) NOT NULL AUTO_INCREMENT,
+  `CompanyName` varchar(256) NOT NULL,
+  `CompanyStart` time NOT NULL,
+  `CompanyStop` time NOT NULL,
+  `CompanyMaxHours` int(11) NOT NULL,
+  `CompanyStartDay` int(11) NOT NULL,
+  `CompanyEndDay` int(11) NOT NULL,
+  `CompanyPayout` varchar(256) NOT NULL,
+  PRIMARY KEY (`CompanyID`),
+  UNIQUE KEY `OrganizationID` (`CompanyID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblcompany`
+--
+
+INSERT INTO `tblcompany` (`CompanyID`, `CompanyName`, `CompanyStart`, `CompanyStop`, `CompanyMaxHours`, `CompanyStartDay`, `CompanyEndDay`, `CompanyPayout`) VALUES
+(6, 'thing', '08:00:00', '22:00:00', 12, 1, 4, 'daily'),
+(7, 'test', '08:00:00', '22:00:00', 12, 3, 7, 'daily');
 
 -- --------------------------------------------------------
 
@@ -78,20 +78,14 @@ INSERT INTO `tblbook` (`EmployeeID`, `DepartmentID`, `BookID`, `StartTime`, `End
 DROP TABLE IF EXISTS `tbldepartment`;
 CREATE TABLE IF NOT EXISTS `tbldepartment` (
   `DepartmentID` int(11) NOT NULL AUTO_INCREMENT,
+  `CompanyID` int(11) NOT NULL,
   `DepartmentName` varchar(256) NOT NULL,
   `DepartmentMinEmployees` int(11) NOT NULL,
   PRIMARY KEY (`DepartmentID`),
-  UNIQUE KEY `DepartmentID` (`DepartmentID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbldepartment`
---
-
-INSERT INTO `tbldepartment` (`DepartmentID`, `DepartmentName`, `DepartmentMinEmployees`) VALUES
-(1, 'Office', 1),
-(2, 'Checkouts', 1),
-(3, 'Shop floor', 1);
+  UNIQUE KEY `DepartmentID` (`DepartmentID`),
+  UNIQUE KEY `OrganizationID_2` (`CompanyID`),
+  KEY `OrganizationID` (`CompanyID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -102,7 +96,7 @@ INSERT INTO `tbldepartment` (`DepartmentID`, `DepartmentName`, `DepartmentMinEmp
 DROP TABLE IF EXISTS `tblemployee`;
 CREATE TABLE IF NOT EXISTS `tblemployee` (
   `EmployeeID` int(11) NOT NULL AUTO_INCREMENT,
-  `OrganizationID` int(11) NOT NULL,
+  `CompanyID` int(11) NOT NULL,
   `EmployeeFirst` varchar(256) NOT NULL,
   `EmployeeLast` varchar(256) NOT NULL,
   `EmployeeType` varchar(256) NOT NULL,
@@ -111,52 +105,17 @@ CREATE TABLE IF NOT EXISTS `tblemployee` (
   `EmployeePassword` varchar(256) NOT NULL,
   PRIMARY KEY (`EmployeeID`),
   UNIQUE KEY `EmployeeID` (`EmployeeID`),
-  KEY `OrganizationID` (`OrganizationID`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+  UNIQUE KEY `OrganizationID_2` (`CompanyID`),
+  KEY `OrganizationID` (`CompanyID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tblemployee`
 --
 
-INSERT INTO `tblemployee` (`EmployeeID`, `OrganizationID`, `EmployeeFirst`, `EmployeeLast`, `EmployeeType`, `EmployeePayrate`, `EmployeeEmail`, `EmployeePassword`) VALUES
-(1, 1, 'Sam', 'Barnes', 'employee', '6.05', 'barnes.samb@gmail.com', 'test'),
-(2, 1, 'Lesley', '', 'admin', '7.83', 'test', 'test'),
-(3, 1, 'Ashleigh', '', 'employee', '7.83', 'test', 'test'),
-(4, 1, 'Michelle', '', 'employee', '7.83', 'test', 'test'),
-(5, 1, 'Jennie', '', 'employee', '7.83', 'test', 'test'),
-(6, 1, 'Peter', '', 'employee', '7.83', 'test', 'test'),
-(7, 1, 'Shani', '', 'employee', '7.83', 'test', 'test'),
-(8, 1, 'Laura', '', 'employee', '7.83', 'test', 'test'),
-(9, 1, 'Amanda', 'BigAss', 'employee', '7.83', 'test1', 'test'),
-(10, 1, 'Tim', '', 'employee', '7.83', 'test', 'test'),
-(11, 1, 'Amy', '', 'employee', '7.83', 'test', 'test'),
-(12, 1, 'Jo', '', 'employee', '7.83', 'test', 'test'),
-(13, 1, 'Ruth', '', 'employee', '7.83', 'test', 'test');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblorganization`
---
-
-DROP TABLE IF EXISTS `tblorganization`;
-CREATE TABLE IF NOT EXISTS `tblorganization` (
-  `OrganizationID` int(11) NOT NULL AUTO_INCREMENT,
-  `OrganizationName` varchar(256) NOT NULL,
-  `OrganizationMaxHours` int(11) NOT NULL,
-  `OrganizationCode` int(11) NOT NULL,
-  `OrganizationPayout` varchar(256) NOT NULL,
-  `OrganizationColor` varchar(256) NOT NULL,
-  PRIMARY KEY (`OrganizationID`),
-  UNIQUE KEY `OrganizationID` (`OrganizationID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblorganization`
---
-
-INSERT INTO `tblorganization` (`OrganizationID`, `OrganizationName`, `OrganizationMaxHours`, `OrganizationCode`, `OrganizationPayout`, `OrganizationColor`) VALUES
-(1, 'Central Convienience', 8, 1458, 'monthly', 'blue');
+INSERT INTO `tblemployee` (`EmployeeID`, `CompanyID`, `EmployeeFirst`, `EmployeeLast`, `EmployeeType`, `EmployeePayrate`, `EmployeeEmail`, `EmployeePassword`) VALUES
+(6, 6, 'thing', 'thing', 'admin', '34.00', 'thing@thing.com', '$2y$10$F1e84tV5MlKI0bDWVT99Rem0ReE7NdrxScjeMRC3j1HbCxX6KMhwS'),
+(7, 7, 'test', 'test', 'admin', '23.00', 'test@test.com', '$2y$10$3KBjJ6Or0ioMel47PTf1/O0teQtGfKcNw34ETJH9xhaFzeikVhxlK');
 
 --
 -- Constraints for dumped tables
@@ -170,10 +129,16 @@ ALTER TABLE `tblbook`
   ADD CONSTRAINT `tblbook_ibfk_2` FOREIGN KEY (`EmployeeID`) REFERENCES `tblemployee` (`EmployeeID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `tbldepartment`
+--
+ALTER TABLE `tbldepartment`
+  ADD CONSTRAINT `tbldepartment_ibfk_1` FOREIGN KEY (`CompanyID`) REFERENCES `tblcompany` (`CompanyID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `tblemployee`
 --
 ALTER TABLE `tblemployee`
-  ADD CONSTRAINT `tblemployee_ibfk_1` FOREIGN KEY (`OrganizationID`) REFERENCES `tblorganization` (`OrganizationID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tblemployee_ibfk_1` FOREIGN KEY (`CompanyID`) REFERENCES `tblcompany` (`CompanyID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
